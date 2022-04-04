f88c5f130d7f983023403c3a43843acb52903a67
Gourav Jawale <velocity.ctc@gmail.com>
2022-04-04 10:18:59 +0530

    xpath

---
+++ src/locators/DynamicXpath.java
@@ -0,0 +1,42 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicXpath {
+	
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "E:\\desktop\\Katraj\\15 Jan\\Selenium\\chromedriver.exe");
+		
+		
+		WebDriver driver = new ChromeDriver();// chrome browser will get open
+		
+		driver.manage().window().maximize();// to maximize the window
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/index.php/dashboard");
+
+
+		WebElement username = driver.findElement(By.xpath("//input[@name='txtUsername']"));
+		
+		
+		username.sendKeys("Admin");
+		
+		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");
+		
+		
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		
+		driver.findElement(By.xpath("//a[@id='menu_admin_viewAdminModule']")).click();
+		
+		for(int i=1; i<40; i++)
+		{
+		driver.findElement(By.xpath("(//input[contains(@id,'ohrmList_chkSelectRecord_')])["+i+"]")).click();
+		
+		}
+		
+		
+	}
+}
---
+++ src/locators/XpathLocator.java
@@ -0,0 +1,37 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathLocator {
+	
+	
+//	syntax:--> //tagname[@attributename = 'attributevalue']
+	
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "E:\\desktop\\Katraj\\15 Jan\\Selenium\\chromedriver.exe");
+		
+		
+		WebDriver driver = new ChromeDriver();// chrome browser will get open
+		
+		driver.manage().window().maximize();// to maximize the window
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/index.php/dashboard");
+		
+		WebElement username = driver.findElement(By.xpath("//input[@name='txtUsername']"));
+		
+		
+		username.sendKeys("Admin");
+		
+		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");
+		
+		
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+	}
+
+	
+
+}
